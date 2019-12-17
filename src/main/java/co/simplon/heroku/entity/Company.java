1adcbfa05fd7ce7d7f88ca4e9dfdd1518a8cc7ac
laude <timothe.laude@gmail.com>
2019-12-17 14:18:31 +0100

    test

--- src/main/java/co/simplon/heroku/entity/Company.java
+++ src/main/java/co/simplon/heroku/entity/Company.java
@@ -10,15 +10,16 @@ import lombok.ToString;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-/*
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@ToString*/
+@ToString
 public class Company implements Serializable{
-    //@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private boolean selected;
