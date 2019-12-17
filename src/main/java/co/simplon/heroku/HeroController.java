f89292b49eaa82207e07d8be935f17ee73dc999e
laude <timothe.laude@gmail.com>
2019-12-17 14:06:41 +0100

    test

--- src/main/java/co/simplon/heroku/HeroController.java
+++ src/main/java/co/simplon/heroku/HeroController.java
@@ -6,8 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HeroController {
     @Autowired
     CompanyRepository companyRepository;
