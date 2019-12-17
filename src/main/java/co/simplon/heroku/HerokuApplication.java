de7a3881d6f649e06cf143996809cadfbb37410c
laude <timothe.laude@gmail.com>
2019-12-17 12:18:24 +0100

    company

--- src/main/java/co/simplon/heroku/HerokuApplication.java
+++ src/main/java/co/simplon/heroku/HerokuApplication.java
@@ -1,5 +1,9 @@
 package co.simplon.heroku;
 
+import co.simplon.heroku.dao.CompanyRepository;
+import co.simplon.heroku.entity.Company;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
@@ -9,7 +13,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @SpringBootApplication
-public class HerokuApplication {
+public class HerokuApplication implements CommandLineRunner {
+	@Autowired
+	private CompanyRepository companyRepository;
 	@RequestMapping("/")
 	@ResponseBody
 	String home() {
@@ -18,6 +24,13 @@ public class HerokuApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HerokuApplication.class, args);
+	}
+	@Override
+	public void run(String... args) throws Exception {
+		companyRepository.save(new Company(null,"Airbus",true));
+		companyRepository.save(new Company(null,"Facebook",true));
+		companyRepository.save((new Company(null,"Google",true)));
+
 	}
 
 }
---
+++ src/main/java/co/simplon/heroku/entity/Company.java
@@ -0,0 +1,25 @@
+package co.simplon.heroku.entity;
+
+
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Company implements Serializable{
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private boolean selected;
+}
