59de9e87afbed3d121ac6594dbb253e6a4040e93
laude <timothe.laude@gmail.com>
2019-12-20 15:27:38 +0100

    dffgt

---
+++ README.md
@@ -0,0 +1,1 @@
+# herokuCompany
--- src/main/java/co/simplon/heroku/HerokuApplication.java
+++ src/main/java/co/simplon/heroku/HerokuApplication.java
@@ -6,11 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
 
 @SpringBootApplication
 public class HerokuApplication implements CommandLineRunner {
